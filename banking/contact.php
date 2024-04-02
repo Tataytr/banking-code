8045399504e6722dc0a3ea88209e56e984a657a6
unknown <Tataytr>
2024-04-02 07:10:59 -0700

    first commit

---
+++ banking/contact.php
@@ -0,0 +1,108 @@
+<?php
+include('header.php')
+?>
+    <!-- Header End -->
+
+    <!-- Banner Start -->
+    <section id="banner-section" class="">
+        <div class="overlay">
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col-md-12 mb-10">
+                        <h1 class="text-light">Contact</h1>
+                    </div>
+    <div class="col-md-12">
+    <ul class="d-flex justify-content-center">
+    <li><a href="index.php">Home</a></li>
+    <li><a class=""> - </a></li>
+    <li><a class="active" href="#!">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Banner End -->
+
+    <!-- Contact Info Start -->
+    <section id="contact-info-section" class="mt-100">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-4 col-lg-4 col-md-6">
+                    <div class="card contact-info-item mt-30">
+                <div class="card-body">
+                            <div class="contact-info-icon text-primary mb-4">
+                                <i class="fa fa-send"></i>
+                </div>
+        <div class="contact-content">
+        <h5 class="mb-3">Contact Address</h5>
+        <p class="m-0">250 Hartford Avenue, 
+        Bellingham MA 2022, USA</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        <div class="col-xl-4 col-lg-4 col-md-6">
+        <div class="card contact-info-item mt-30">
+        <div class="card-body">
+        <div class="contact-info-icon text-primary mb-4">
+        <i class="fa fa-hourglass"></i>
+        </div>
+
+        <div class="contact-content">
+         <h5 class="mb-3">Timing</h5>
+         <p class="m-0"><span class="fw-bold">Mon - Sat:</span> 09:00 AM - 06:00 PM</p>
+        <p class="m-0"><span class="fw-bold">Sunday:</span> 12:00 PM - 6:00 PM</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6">
+                <div class="card contact-info-item mt-30">
+                        <div class="card-body">
+                <div class="contact-info-icon text-primary mb-4">
+                <i class="fa fa-phone"></i>
+                </div>
+                <div class="contact-content">
+                <h5 class="mb-3">Let's Talk</h5>
+                        
+                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Contact Info End -->
+
+    <!-- Contact Start -->
+    <section id="contact-form-section" class="mt-120">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="section-title text-center mb-50">
+                <h6>Book Appoinment Now!</h6>
+                <h2>Have Queries Before the Appointment?</h2>
+                <p> Please send a message to support@imperialpremiumbank.online and we will reply within 24 hours.</p>
+                    </div>
+                </div>
+            </div>
+            
+    <div class="col-lg-5 my-3">
+    <div class="g-map">
+    <div class="card card-body">
+    <div class="ratio ratio-1x1">
+
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2959.5916530885497!2d-71.46755128515001!3d42.116215579203974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e471e2abbb9e93%3A0xb0539849cec2161b!2s250%20Hartford%20Ave%2C%20Bellingham%2C%20MA%2002019%2C%20USA!5e0!3m2!1sen!2sng!4v1630800294967!5m2!1sen!2sng" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Contact End -->
+
+    <?php
+include('footer.php')
+?>
---
+++ banking/footer.php
@@ -0,0 +1,179 @@
+  <!-- Footer Start  -->
+  <footer id="footer-section" class="mt-115">
+    <!-- Start Footer Top -->
+    <div class="footer-top">
+        <div class="container">
+<div class="row">
+<div class="col-lg-4 col-md-6 col-12 col-sm-6">
+<div class="footer-cmn f-activity wow fadeInUp" data-wow-delay=".2s">
+
+<h6>GET IN TOUCH</h6>
+<h5>Subscribe Our Newsletter</h5>
+<p>Making the world a better place through constructing elegant hierarchies. 
+Innovations enabled by the TREASURY 
+Imperial Premium Bank   results in 
+products that assist workers.</p>
+
+<div class="subscribe mt-30">
+            <form>
+<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Email">
+<button type="button" class="btn btn-primary">
+<i class="fa fa-send"></i></button>
+                            </form>
+                        </div>
+    <ul class="social d-flex mt-30">
+    <li><a href="javascript:void(0)"><i class="fa fa-facebook"></i></a></li>
+    <li><a href="javascript:void(0)"><i class="fa fa-twitter"></i></a></li>
+    <li><a href="javascript:void(0)"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+<div class="col-lg-4 col-md-6 col-12 col-sm-6">
+<div class="footer-cmn f-address wow fadeInUp" data-wow-delay=".4s">
+<h6>Location</h6>
+<h5>Head Office</h5>
+<p>301 East Water Street, Charlottesville, VA 22904 Virginia.</p>
+<ul class="mt-20">
+<li><a href="javascript:void(0)"><i class="flaticon-mobile"></i>+496</a></li>
+
+<li><a href="javascript:void(0)"><i class="flaticon-mail"></i> 
+support@hacvestfinance.com
+</a></li>
+                             
+</ul>
+                    </div>
+</div>
+<div class="col-lg-2 col-md-6 col-12 col-sm-6">
+<div class="footer-cmn f-solutions f-links wow fadeInUp" data-wow-delay=".6s">
+<h5>Services</h5>
+<ul>
+<li><a href="javascript:void(0)">Business Loan</a></li>
+<li><a href="javascript:void(0)">Personal Loan</a></li>
+<li><a href="javascript:void(0)">Home Loan</a></li>
+<li><a href="javascript:void(0)">Car Lone</a></li>
+<li><a href="javascript:void(0)">Land Loan</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+<div class="col-lg-2 col-md-6 col-12 col-sm-6">
+<div class="footer-cmn f-legal f-links wow fadeInUp" data-wow-delay=".8s">
+<h5>Legal</h5>
+        <ul>
+
+<li><a href="javascript:void(0)">Terms & Conditions</a></li>
+<li><a href="javascript:void(0)">Privacy Policy</a>
+</li>
+<li><a href="javascript:void(0)">Customer Relations</a></li>
+<li><a href="javascript:void(0)">Innovation</a></li>
+<li><a href="javascript:void(0)">Quick Contact</a></li>
+<li><a href="javascript:void(0)">Apply Online</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ Footer Top End  -->
+    <div class="copyright text-center mt-50">
+        <div class="container">
+        <div class="row">
+        <div class="col-12">
+        <p class="copyright-text mb-0">All Rights Reserved 2022 <a href="javascript:void(0)/" rel="nofollow">Imperial Premium Bank </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer End -->
+
+<!-- Scroll Top Start -->
+<a href="#" class="scroll-top">
+    <i class="lni lni-chevron-up"></i>
+</a>
+<!-- Scroll Top End -->
+<script src="//code.tidio.co/tieaxaqkfiu1wyvlzqykmw6uvcqhllyk.js" async></script>
+<!-- JS here -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/tiny-slider.js"></script>
+<script src="js/glightbox.min.js"></script>
+<script src="js/main.js"></script>
+<script>
+    /* Client
+    -----------------------*/
+    tns({
+        container: '.client-active',
+        autoplay: true,
+        autoplayButtonOutput: false,
+        mouseDrag: true,
+        gutter: 15,
+        nav: false,
+        controls: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            540: {
+                items: 2,
+            },
+            768: {
+                items: 3,
+            },
+            992: {
+                items: 4,
+            },
+            1200: {
+                items: 5,
+            }
+        }
+    });
+    /* Testimonial
+    -----------------------*/ 
+    tns({
+        container: '.testimonial-slider',
+        items: 3,
+        slideBy: 'page',
+        autoplay: false,
+        mouseDrag: true,
+        gutter: 0,
+        nav: true,
+        controls: false,
+        controlsText: ['<i class="lni lni-arrow-left"></i>', '<i class="lni lni-arrow-right"></i>'],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            540: {
+                items: 1,
+            },
+            768: {
+                items: 1,
+            },
+            992: {
+                items: 2,
+            },
+            1170: {
+                items: 2,
+            }
+        }
+    });
+    /* glightbox
+    -----------------------*/
+    GLightbox({
+        'href': 'https://www.youtube.com/watch?v=UwPrJmOxPeI',
+        'type': 'video',
+        'source': 'youtube',
+        'width': 900,
+        'autoplayVideos': true,
+    });
+</script>
+
+
+<!--End of Tawk.to Script-->
+</body>
+
+<!-- Mirrored from royalalliancetrustbank.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 10 Jul 2022 17:46:37 GMT -->
+</html>
